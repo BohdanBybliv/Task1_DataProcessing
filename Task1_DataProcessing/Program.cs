9626c9196f1e2db9cd9f56c2faaea0f17dcc48fe
Bohdan Bybliv <bohdanbybliv7@gmail.com>
2023-02-19 22:22:58 +0200

    Add file watcher

--- Task1_DataProcessing/Program.cs
+++ Task1_DataProcessing/Program.cs
@@ -1,111 +1,17 @@
-﻿using System.Configuration;
-using System.Text.Json;
-using Task1_DataProcessing.Models;
-
-namespace Task1_DataProcessing
+﻿namespace Task1_DataProcessing
 {
     internal static class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
         {
-            var appSettings = ConfigurationManager.AppSettings;
-
-            string? folderA = appSettings.Get("folder_a");
-            string? folderB = appSettings.Get("folder_b");
-            string fileName = "raw_data.txt";
-
-            List<Transform> transforms = new List<Transform>();
-
-            using (StreamReader reader = new StreamReader(folderA + fileName))
+            using (Watcher watcher = new Watcher())
             {
-                var rows = reader.ReadToEnd().Split('\n');
-
-                foreach (var row in rows)
-                {
-                    Console.WriteLine(row);
-
-                    var items = row.Replace(",", "").Split('"');
-
-                    string name = items[0].Remove(items[0].Length - 1);
-
-                    string city = items[1].Substring(0, items[1].IndexOf(' '));
-
-                    decimal payment;
-                    DateTime date;
-                    long accountNumber;
-                    string serviceName;
-
-                    if (items.Length == 3)
-                    {
-                        var values = items[2].Split(' ');
-
-                        payment = Convert.ToDecimal(values[1].Replace(".", ","));
-
-                        date = DateTime.ParseExact(values[2], "yyyy-dd-MM", null);
-
-                        accountNumber = Convert.ToInt64(values[3]);
-
-                        serviceName = values[4].Replace(" ", "").Replace("\r", "");
-                    }
-                    else
-                    {
-                        var values = items[2].Split(' ');
-
-                        payment = Convert.ToDecimal(values[1].Replace(".", ","));
-
-                        date = DateTime.ParseExact(values[2], "yyyy-dd-MM", null);
-
-                        accountNumber = Convert.ToInt64(items[3]);
-
-                        serviceName = items[4].Replace(" ", "").Replace("\r", "");
-                    }
-
-                    var transform = transforms.FirstOrDefault(f => f.City == city);
-                    Payer payer = new Payer(name, payment, date, accountNumber);
-
-                    if (transform is null)
-                    {
-                        Service service = new Service();
-                        service.Name = serviceName;
-                        service.Payers.Add(payer);
-                        service.Total += payment;
-
-                        transform = new Transform();
-                        transform.City = city;
-                        transform.Services.Add(service);
-                        transform.Total += payment;
-
-                        transforms.Add(transform);
-                    }
-                    else
-                    {
-                        var service = transform.Services.FirstOrDefault(s => s.Name == serviceName);
-
-                        if (service is null)
-                        {
-                            service = new Service();
-                            service.Name = serviceName;
-                            service.Payers.Add(payer);
-                            service.Total += payment;
-
-                            transform.Services.Add(service);
-                            transform.Total += payment;
-                        }
-                        else
-                        {
-                            service.Payers.Add(payer);
-                            service.Total += payment;
-
-                            transform.Total += payment;
-                        }
-                    }
-                }
-            }
-
-            using (FileStream fs = new FileStream(folderB + "output1.json", FileMode.OpenOrCreate))
-            {
-                await JsonSerializer.SerializeAsync(fs, transforms);
-                Console.WriteLine("Data has been saved to file");
+                watcher.Start();
+                Console.WriteLine("Press 'r' to reset, 's' to stop and save log.");
+                string? input = Console.ReadLine();
+                if (input == "r") watcher.Reset();
+                else if (input == "s") watcher.Stop();
+                Console.ReadKey();
             }
         }
     }
---
+++ Task1_DataProcessing/Watcher.cs
@@ -0,0 +1,84 @@
+﻿using System.Configuration;
+using Task1_DataProcessing.FileParsers.CsvFileParser;
+using Task1_DataProcessing.FileParsers.TxtFileParser;
+using Task1_DataProcessing.Models;
+
+namespace Task1_DataProcessing
+{
+    internal class Watcher : IDisposable
+    {
+        private readonly string? _folderA;
+        private readonly FileSystemWatcher _watcher;
+        private readonly ITxtFileParser _txtParser;
+        private readonly ICsvFileParser _csvParser;
+        private readonly Logger _logger;
+        public Watcher()
+        {
+            _folderA = ConfigurationManager.AppSettings.Get("folder_a");
+            _watcher = new FileSystemWatcher(_folderA);
+            _logger = new Logger();
+            _txtParser = new TxtFileParser(_logger);
+            _csvParser = new CsvFileParser(_logger);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            _watcher.Dispose();
+        }
+        public void Start()
+        {
+            Console.WriteLine("Start application...");
+
+            _watcher.NotifyFilter = NotifyFilters.FileName
+                    | NotifyFilters.DirectoryName
+                    | NotifyFilters.LastWrite
+                    | NotifyFilters.LastAccess
+                    | NotifyFilters.Size;
+
+            _watcher.Filters.Add("*.txt");
+            _watcher.Filters.Add("*.csv");
+
+            _watcher.Changed += OnChanged;
+            _watcher.EnableRaisingEvents = true;
+        }
+        public void Reset()
+        {
+            _logger.ResetLog();
+        }
+        public void Stop()
+        {
+            _logger.SaveLog();
+            _watcher.Changed -= OnChanged;
+            _watcher.Dispose();
+        }
+        private async void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            string path = e.FullPath;
+            Console.WriteLine($"Added new file: {e.Name}");
+
+            FileParserMethodResult result;
+            if (Path.GetExtension(path) == ".txt") result = await _txtParser.ParseFile(path);
+            else result = await _csvParser.ParseFile(path);
+
+            if (result.IsSuccess)
+            {
+                Console.WriteLine(result.Message);
+                _logger.ParsedFiles++;
+                _logger.ParsedLines += result.ParsedLines;
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+                _logger.ParsedFiles++;
+                _logger.ParsedLines += result.ParsedLines;
+                _logger.FoundErrors += result.FoundErrors;
+                _logger.InvalidFiles.Add(e.Name);
+            }
+        }
+    }
+}
