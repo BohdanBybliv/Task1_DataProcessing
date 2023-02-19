cab934aa9b10351e130b6aadb0fbec2fca223a67
Bohdan Bybliv <bohdanbybliv7@gmail.com>
2023-02-19 22:19:09 +0200

    Add method content result

---
+++ Task1_DataProcessing/Models/FileParserMethodResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1_DataProcessing.Models
+{
+    public class FileParserMethodResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public int ParsedLines { get; set; }
+        public int FoundErrors { get; set; }
+        public FileParserMethodResult(bool isSuccess, string message, int parsedLines, int fountErrors = 0)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+            ParsedLines = parsedLines;
+            FoundErrors = fountErrors;
+        }
+    }
+}
